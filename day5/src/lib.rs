f35e2d1a70fb4c9c18261cf9f740bad92860bc02
Tom Schollenberger <tss2344@g.rit.edu>
2025-12-05 00:55:27 -0500

    reuse code

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -3,49 +3,7 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-pub fn part1(mut reader: BufReader<File>) -> usize {
-    let mut range_set = Vec::new();
-
-    loop {
-        let mut id_range = String::new();
-        reader.read_line(&mut id_range).unwrap();
-        let id_range = id_range.trim();
-
-        if id_range.is_empty() {
-            break;
-        }
-
-        let split: Vec<&str> = id_range.split("-").collect();
-        let start: i64 = split[0].trim().parse().unwrap();
-        let end: i64 = split[1].trim().parse().unwrap();
-
-        range_set.push((start, end))
-    }
-
-    let mut count = 0;
-    loop {
-        let mut id = String::new();
-        let Ok(_) = reader.read_line(&mut id) else {
-            break;
-        };
-        let id = id.trim();
-        if id.is_empty() {
-            break;
-        }
-
-        let id: i64 = id.parse().unwrap();
-        for (start, stop) in &range_set {
-            if id >= *start && id <= *stop {
-                count += 1;
-                break;
-            }
-        }
-    }
-
-    count
-}
-
-pub fn part2(mut reader: BufReader<File>) -> i64 {
+fn get_ranges(reader: &mut BufReader<File>) -> Vec<(i64, i64)> {
     let mut range_set = Vec::new();
 
     loop {
@@ -80,9 +38,40 @@ pub fn part2(mut reader: BufReader<File>) -> i64 {
         }
     }
 
+    range_set
+}
+
+pub fn part1(mut reader: BufReader<File>) -> usize {
+    let range_set = get_ranges(&mut reader);
+
+    let mut count = 0;
+    loop {
+        let mut id = String::new();
+        let Ok(_) = reader.read_line(&mut id) else {
+            break;
+        };
+        let id = id.trim();
+        if id.is_empty() {
+            break;
+        }
+
+        let id: i64 = id.parse().unwrap();
+        for (start, stop) in &range_set {
+            if id >= *start && id <= *stop {
+                count += 1;
+                break;
+            }
+        }
+    }
+
+    count
+}
+
+pub fn part2(mut reader: BufReader<File>) -> i64 {
+    let range_set = get_ranges(&mut reader);
+
     let mut count = 0;
     for range in range_set {
-        println!("{range:?}");
         count += range.1 - range.0 + 1;
     }
 
